2b158ba98f35086a1db8507a67c074571b9f6e33
David Eduardo da Silva Correia <david.correia@bempromotora.onmicrosoft.com>
2024-03-15 09:32:11 -0300

    add pastar docker

---
+++ back-db/ajusteDatabase.sql
@@ -0,0 +1,3 @@
+/* https://www.softwaredeveloper.blog/initialize-mssql-in-docker-container */
+CREATE DATABASE CoinMarketCap;
+GO
---
+++ back-db/entrypoint.sh
@@ -0,0 +1,2 @@
+# Run Microsoft SQl Server and initialization script (at the same time)
+/src/app/run-initialization.sh & /opt/mssql/bin/sqlservr
